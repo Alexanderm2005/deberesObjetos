099dc290969a76622484a3e3945b9bf27ce1e7ef
Alexanderm2005 <gamarcillo@sudamericano.edu.ec>
2024-05-21 12:32:24 -0500

    Deberes Objeto
    Alexander Marcillo

---
+++ 2.1-Comunicacion/T.A/chocolate-factory-angular/src/app/components/father/father.component.ts
@@ -0,0 +1,39 @@
+// father.component.ts
+import { Component, AfterContentInit, ContentChild, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { ChildComponent } from '../child/child.component';
+
+@Component({
+  selector: 'app-father',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NzButtonModule, NzCardModule, ChildComponent],
+  templateUrl: './father.component.html',
+  styleUrls: ['./father.component.css']
+})
+export class FatherComponent implements AfterViewInit, AfterContentInit {
+  parentMessageInput = 'Necesito un reporte general de toda la mercancìa de hoy';
+  childMessageEventOutput = '';
+
+  @ViewChild(ChildComponent) childComponent!: ChildComponent;
+  @ContentChild('projectedContent', { static: true }) projectedContent!: ElementRef;
+
+  receiveMessage($event: string) {
+    this.childMessageEventOutput = $event;
+  }
+
+  ngAfterViewInit() {
+    Promise.resolve().then(() => {
+      this.childComponent.parentMessageViewChild = 'De forma remota voy hacer uso del exoesqueleto(Variables del Hijo) ' +
+        'que tienes instalado comprobar la nueva mercaderìa de hoy';
+    });
+  }
+
+  ngAfterContentInit() {
+    if (this.projectedContent) {
+      console.log('El contenido proyectado es:', this.projectedContent.nativeElement.textContent);
+    }
+  }
+}
